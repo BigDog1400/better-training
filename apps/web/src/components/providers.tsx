d8bc4d6d2ca531a21d357bf0390b0d83bd12314c
Isaac <const.isaac@gmail.com>
2025-08-03 17:18:17 -0400

    feat: capitalize exercise names and set light theme as default

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -11,9 +11,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     <QueryClientProvider client={queryClient}>
     <ThemeProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       disableTransitionOnChange
-      enableSystem
+      enableSystem={false}
     >
       {children}
       <Toaster richColors />
